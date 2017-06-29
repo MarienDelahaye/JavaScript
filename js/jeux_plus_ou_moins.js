a3015255ac1e54d40cccba0b08792e96a66430f6
Delahaye Marien <delahaye.marien@gmail.com>
2017-06-29 14:26:36 +0200

    index.html edited + Add js2.html and jeux_plus_ou_moins.js

---
+++ js/jeux_plus_ou_moins.js
@@ -0,0 +1,18 @@
+var nb = nb_aleatoire(1, 100);  // nb a deviner
+var cpt = 0;    // nb de coups
+var saisie;
+var msg = 'Le nombre à deviner est compris entre 1 et 100.';
+
+do
+{
+     saisie = prompt(msg);
+     cpt++;
+     // message a afficher au prochain tour :
+     if(saisie > nb)
+          msg = "C'est moins";
+     else
+          msg = "C'est plus";
+}
+while(saisie != nb);
+
+alert("Bravo, tu as gagne en " + cpt + " coups !");
