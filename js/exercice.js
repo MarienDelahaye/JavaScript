3fdf1e94b14cf6b7b4035a1437593bb83993ba2e
Delahaye Marien <delahaye.marien@gmail.com>
2017-06-30 09:59:16 +0200

    exercice.js edited

--- js/exercice.js
+++ js/exercice.js
@@ -2,28 +2,37 @@ var choixUtilisateur = prompt("Choisissez-vous pierre, feuille, ou ciseaux ?");
 var choixOrdi = Math.random();
 if (choixOrdi < 0.34) {
 choixOrdi = "pierre";
-} else if(choixOrdi <= 0.67) {
+}
+else if(choixOrdi <= 0.67) {
 choixOrdi = "feuille";
-} else {
+}
+else {
 choixOrdi = "ciseaux";
-} console.log("Ordinateur : " + choixOrdi);
+}
+console.log("Ordinateur : " + choixOrdi);
 var comparer = function(choix1, choix2){
-if (choix1===choix2){
+if (choix1===choix2)
+{
 return "Egalité !";
 }
-else if (choix1==="pierre"){
-if (choix2==="ciseaux"){
+else if (choix1==="pierre")
+{
+if (choix2==="ciseaux")
+{
 return "pierre gagne !";
 }
 else{
 return "feuille gagne !";
 }
 }
-if (choix1===choix2){
+if (choix1===choix2)
+{
 return "Egalité !";
 }
-else if (choix1==="feuille"){
-if (choix2==="pierre"){
+else if (choix1==="feuille")
+{
+if (choix2==="pierre")
+{
 return "feuille gagne !";
 }
 else{
@@ -33,12 +42,18 @@ return "ciseaux gagne !";
 if (choix1===choix2){
 return "Egalité !";
 }
-else if (choix1==="ciseaux"){
-if (choix2==="feuille"){
+else if (choix1==="ciseaux")
+{
+if (choix2==="feuille")
+{
 return "ciseaux gagne !";
 }
-else{
+else
+{
 return "pierre gagne !";
 }
 }
 }
+comparer(choixUtilisateur, choixOrdi);
+alert("Tu as choisis: " + choixUtilisateur);
+alert("L'ordinateur a choisis: " + choixOrdi);
