9b4086c01b736318482330907fec276425640cd1
Delahaye Marien <delahaye.marien@gmail.com>
2017-07-04 09:17:23 +0200

    Final editing exercice.js and js1.html

--- js/exercice.js
+++ js/exercice.js
@@ -1,60 +1,37 @@
-var choixUtilisateur = prompt("Choisissez-vous pierre, feuille, ou ciseaux ?");
-var choixOrdi = Math.random();
-if (choixOrdi < 0.34) {
-choixOrdi = "pierre";
+var scoreO=0;
+var scoreU=0;
+
+while(scoreO!=3 && scoreU!=3){
+
+ var user=prompt("pierre feuille ou ciseaux");
+  var ordi= Math.random();
+
+if (ordi<=0.34){
+  ordi=("pierre")
 }
-else if(choixOrdi <= 0.67) {
-choixOrdi = "feuille";
+else  if (ordi<=0.67 && ordi>0.34){
+    ordi=("feuille");
+    }
+      else{
+      ordi=("ciseaux");
+    }
+      alert("l'ordi à choisi : " + ordi);
+
+ if (user===ordi) {
+    alert("egalité");
+      }
+
+ else  if((user==="pierre" && ordi==="ciseaux")
+      || (user==="ciseaux" && ordi==="feuille")
+      || (user==="feuille" && ordi==="pierre")){
+        scoreU++ ;
+        alert("joueur gagne, ton score est : " + scoreU + " et le score de l'ordinateur est : " + scoreO );
+      }
+        else {
+          scoreO++;
+          alert("ordi gagne, son score est : " + scoreO + " et le score du joueur est : " + scoreU);
+        }
+
 }
-else {
-choixOrdi = "ciseaux";
-}
-console.log("Tu as choisis : " + choixUtilisateur); // Ce que l'utilisateur choisis s'affiche dans la console
-console.log("L'ordinateur a choisis: " + choixOrdi); // Ce que l'ordinateur choisis s'affiche dans la console
-alert("Tu as choisis: " + choixUtilisateur); // Ce que  l'utilisateur choisis mais s'affiche sous forme d'alert
-alert("L'ordinateur a choisis: " + choixOrdi); // Ce que l'ordinateur choisis mais s'affiche sous forme d'alert
-var comparer = function(choix1, choix2){
-if (choix1===choix2)
-{
-return "Egalité !";
-}
-else if (choix1==="pierre")
-{
-if (choix2==="ciseaux")
-{
-return "pierre gagne !";
-}
-else{
-return "feuille gagne !";
-}
-}
-if (choix1===choix2)
-{
-return "Egalité !";
-}
-else if (choix1==="feuille")
-{
-if (choix2==="pierre")
-{
-return "feuille gagne !";
-}
-else{
-return "ciseaux gagne !";
-}
-alert ("ciseaux gagne !");
-}
-if (choix1===choix2){
-return "Egalité !";
-}
-else if (choix1==="ciseaux")
-{
-if (choix2==="feuille")
-{
-return "ciseaux gagne !";
-}
-else
-{
-return "pierre gagne !";
-}
-}
-}
+
+alert ("ordinateur " + scoreO +" "+"vous " +scoreU +"");
