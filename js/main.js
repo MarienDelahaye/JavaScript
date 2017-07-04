51cc9126b59cd07af80d7011cb6edc0dd1734f3a
Delahaye Marien <delahaye.marien@gmail.com>
2017-07-04 09:44:44 +0200

    Moving exercice.js code to main.js and deleted some files

---
+++ js/main.js
@@ -0,0 +1,37 @@
+var scoreO=0;
+var scoreU=0;
+
+while(scoreO!=3 && scoreU!=3){
+
+ var user=prompt("pierre feuille ou ciseaux");
+  var ordi= Math.random();
+
+if (ordi<=0.34){
+  ordi=("pierre")
+}
+else  if (ordi<=0.67 && ordi>0.34){
+    ordi=("feuille");
+    }
+      else{
+      ordi=("ciseaux");
+    }
+      alert("l'ordi à choisi : " + ordi);
+
+ if (user===ordi) {
+    alert("egalité");
+      }
+
+ else  if((user==="pierre" && ordi==="ciseaux")
+      || (user==="ciseaux" && ordi==="feuille")
+      || (user==="feuille" && ordi==="pierre")){
+        scoreU++ ;
+        alert("joueur gagne, ton score est : " + scoreU + " et le score de l'ordinateur est : " + scoreO );
+      }
+        else {
+          scoreO++;
+          alert("ordi gagne, son score est : " + scoreO + " et le score du joueur est : " + scoreU);
+        }
+
+}
+
+alert ("ordinateur " + scoreO +" "+"vous " +scoreU +"");
